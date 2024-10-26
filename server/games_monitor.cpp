c64e49506f27ce23db8109a92443b876ba2831dc
Lautaro <lpedrozo@fi.uba.ar>
2024-10-25 22:08:55 -0300

    Implemento clase para monitorear juegos

---
+++ server/games_monitor.cpp
@@ -0,0 +1,42 @@
+#include "games_monitor.h"
+#include <cstdio>
+
+
+// TODO: el mutex no es necesario??, El acceptor es el unico que va a agregar players
+// y el acceptor es unico
+void GamesMonitor::add_player(ServerClient* player) {
+    int cmd = player->receive_cmd();
+    if (cmd == 1) { // CREAR
+        Game* game = create_game();
+        game->add_player(player);
+    } else if (cmd == 2) { // UNIRSE
+        player->send_games_info(list_lobbies());
+        // esperar por el id del juego
+        int id = player->receive_cmd();
+        Game* game = map_games[id];
+        game->add_player(player);
+    } 
+}
+
+std::vector<int> GamesMonitor::list_lobbies() {
+    std::vector<int> lobbies;
+    printf("Listando lobbies\n");
+    for (auto& game: map_games) {
+        if (game.second->is_open()) {
+            printf("Lobby abierto: %d\n", game.first);
+            lobbies.push_back(game.first);
+        }
+    }
+    lobbies.push_back(-1);
+    return lobbies;
+}
+
+Game* GamesMonitor::create_game() {
+    Game* game = new Game(id);
+    printf("Juego creado con id: %d\n", id);
+    map_games.emplace(id, game);
+    id++;
+    return game;
+}
+
+//void GamesMonitor::delete_game(int id) {}
---
+++ server/games_monitor.h
@@ -0,0 +1,26 @@
+#ifndef GAMES_MONITOR_H
+#define GAMES_MONITOR_H
+
+#include <map>
+#include <mutex>
+#include "server_client.h"
+#include "game.h"
+
+class GamesMonitor {
+private:
+private:
+    std::mutex m;
+    std::map<int, Game*> map_games;
+    int id = 0;
+public:
+void add_player(ServerClient* player);
+
+Game* create_game();
+
+void delete_game(int id);
+
+std::vector<int> list_lobbies();
+
+};
+
+#endif
