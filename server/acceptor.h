5198dfb7ae61c101c0ad89899c1099a539d4256e
Lautaro <lpedrozo@fi.uba.ar>
2024-10-25 22:07:31 -0300

    Modifico constructor de Acceptor e implemento Monitor de Juegos

--- server/acceptor.h
+++ server/acceptor.h
@@ -3,26 +3,18 @@
 
 #include <list>
 
-// TODO: Todos los includes necesarios
-#include "common/blocking_queue.h"
-#include "common/map_dto.h"
 #include "common/socket.h"
 #include "common/thread.h"
 
 #include "server_client.h"
-#include "list_monitor.h"
 
 class Acceptor: public Thread {
 private:
     Socket& sk;
-    std::list<ServerClient*> clients;
-    Queue<struct action>& gameloop_q;
-
-    QueueListMonitor& sv_msg_queues;
-    Map& map;
+    std::list<ServerClient*> players;
 
 public:
-    Acceptor(Socket& sk, Queue<struct action>& gameloop_q, QueueListMonitor& sv_msg_queues, Map& map);
+    Acceptor(Socket& sk);
     void run() override;
     void reap_dead();
     void kill_all();
