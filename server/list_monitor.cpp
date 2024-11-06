726c23556c3a3ef9b6d4d42157d3d0cb31176f5f
Lautaro <lpedrozo@fi.uba.ar>
2024-11-06 14:39:49 -0300

    Elimino printf

--- server/list_monitor.cpp
+++ server/list_monitor.cpp
@@ -9,8 +9,6 @@ void QueueListMonitor::add_element(Queue<Snapshot>* queue, int player_id) {
 void QueueListMonitor::remove_element(int player_id) {
     std::lock_guard<std::mutex> lock(m);
     list.remove_if([player_id](const std::pair<Queue<Snapshot>*, int>& pair) {
-        bool data = pair.second == player_id;
-        printf("eliminando queue en monitor por player id bool %d\n", data);
         return pair.second == player_id;
     });
 }
