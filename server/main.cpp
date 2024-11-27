583e5ede098871f9c1dbb701bf1faf40b685fcbd
Lautaro <lpedrozo@fi.uba.ar>
2024-11-27 12:32:12 -0300

    update mensaje error

--- server/main.cpp
+++ server/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Argumentos invalidos usar: " << argv[0] << " <puerto>" << std::endl;
+        std::cerr << "Bad call. Usage:  " << argv[0] << " <port>" << std::endl;
         return 1;
     }
 
