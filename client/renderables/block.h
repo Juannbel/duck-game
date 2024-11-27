2b9728e8b926c4be8bb50bcdcfceb1664fe10d68
Juan Bel <juanbel1905@gmail.com>
2024-11-27 18:58:05 -0300

    Simplifico el tema de los bloques no solidos, ahora no se cambia el alpha de la textura cada vez que se renderiza, sino que directamente se carga una de las texturas con el alpha sin modificar y otra modificado

--- client/renderables/block.h
+++ client/renderables/block.h
@@ -7,23 +7,17 @@
 #include <SDL_blendmode.h>
 
 #include "client/camera.h"
-#include "common/config.h"
 
-static Config& conf = Config::get_instance();
-
-const static uint8_t NON_SOLID_OPACITY = conf.get_non_solid_opacity();
 class RenderableBlock {
 private:
     SDL2pp::Rect src_rect;
     SDL2pp::Rect dst_rect;
     std::shared_ptr<SDL2pp::Texture> texture;
-    bool solid;
 
 public:
     RenderableBlock(SDL2pp::Rect src_rect, SDL2pp::Rect dst_rect,
-                    std::shared_ptr<SDL2pp::Texture> texture, bool solid):
-            src_rect(src_rect), dst_rect(dst_rect), texture(texture), solid(solid) {
-            }
+                    std::shared_ptr<SDL2pp::Texture> texture):
+            src_rect(src_rect), dst_rect(dst_rect), texture(texture) {}
 
     void render(SDL2pp::Renderer& renderer, Camera& camera) {
         if (!camera.is_rect_visible(dst_rect))
@@ -31,17 +25,7 @@ public:
         SDL2pp::Rect d_rect = dst_rect;
 
         camera.transform_rect(d_rect);
-
-        if (!solid) {
-            texture->SetAlphaMod(NON_SOLID_OPACITY);
-        }
-
         renderer.Copy(*texture, src_rect, d_rect);
-
-        if (!solid) {
-            texture->SetAlphaMod();
-        }
-
     }
 };
 
