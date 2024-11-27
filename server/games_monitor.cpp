b9405d68db1d46460a3c0692b50cd2783406d80e
Lautaro <lpedrozo@fi.uba.ar>
2024-11-27 15:56:54 -0300

    implemento manejo de punteros game con unique_ptr

--- server/games_monitor.cpp
+++ server/games_monitor.cpp
@@ -11,8 +11,8 @@
 GameInfo GamesMonitor::player_create_game(const int id_player, Queue<Snapshot>& player_sender_queue,
                                           const std::vector<std::string>& players_names) {
     std::lock_guard<std::mutex> lck(m);
-    Game* game = create_game(players_names[0], id_player);
-    GameInfo game_info = game->add_player(id_player, player_sender_queue, players_names);
+    const int id_game = create_game(players_names[0], id_player);
+    const GameInfo game_info = map_games[id_game]->add_player(id_player, player_sender_queue, players_names);
     return game_info;
 }
 
@@ -20,24 +20,23 @@ GameInfo GamesMonitor::player_join_game(const int id_player, const int id_game,
                                         Queue<Snapshot>& player_sender_queue,
                                         const std::vector<std::string>& players_names) {
     std::lock_guard<std::mutex> lck(m);
-    if (map_games.find(id_game) == map_games.end()) {
+    if (!map_games.contains(id_game)) {
         return {INVALID_GAME_ID, INVALID_DUCK_ID, INVALID_DUCK_ID};
     }
-    Game* game = map_games[id_game];
-    return game->add_player(id_player, player_sender_queue, players_names);
+
+    return map_games[id_game]->add_player(id_player, player_sender_queue, players_names);
 }
 
 void GamesMonitor::start_game(int id_game) {
     std::lock_guard<std::mutex> lck(m);
-    Game* game = map_games[id_game];
-    game->start();
+    map_games[id_game]->start();
 }
 
 std::vector<LobbyInfo> GamesMonitor::list_lobbies() {
     std::lock_guard<std::mutex> lck(m);
     std::vector<LobbyInfo> lobbies;
     for (auto& game: map_games) {
-        Game* game_ptr = game.second;
+        Game* game_ptr = game.second.get();
         if (game_ptr && game_ptr->is_open()) {
             lobbies.push_back(game.second->get_info());
         }
@@ -45,34 +44,30 @@ std::vector<LobbyInfo> GamesMonitor::list_lobbies() {
     return lobbies;
 }
 
-LobbyInfo GamesMonitor::get_lobby_info(int32_t id_game) {
+LobbyInfo GamesMonitor::get_lobby_info(const int32_t id_game) {
     std::lock_guard<std::mutex> lck(m);
-    if (map_games.find(id_game) == map_games.end()) {
+    if (!map_games.contains(id_game)) {
         return LobbyInfo{};
     }
-    Game* game = map_games[id_game];
-    return game->get_info();
+    return map_games[id_game]->get_info();
 }
 
-Game* GamesMonitor::create_game(const std::string& creator_name, const int id_player) {
-    // TODO: podrían haber utilizado smart pointers para evitar new/delete
-    Game* game = new Game(id, creator_name, id_player);
-    map_games.emplace(id, game);
+int GamesMonitor::create_game(const std::string& creator_name, const int id_player) {
+    std::unique_ptr<Game> game = std::make_unique<Game>(id, creator_name, id_player);
+    map_games.emplace(id, std::move(game));
     id++;
-    return game;
+    return id - 1;
 }
 
 Queue<action>* GamesMonitor::get_gameloop_q(const int id_game) {
     std::lock_guard<std::mutex> lck(m);
-    Game* game = map_games[id_game];
-    return &game->get_gameloop_queue();
+    return &map_games[id_game]->get_gameloop_queue();
 }
 
 void GamesMonitor::remove_player(const int id_game, const int id_player) {
     std::lock_guard<std::mutex> lck(m);
-    if (Game* game = map_games[id_game]) {
+    if (Game* game = map_games[id_game].get()) {
         if (game->delete_player(id_player)) {
-            delete game;
             map_games.erase(id_game);
         }
     }
@@ -80,9 +75,6 @@ void GamesMonitor::remove_player(const int id_game, const int id_player) {
 
 void GamesMonitor::delete_games() {
     std::lock_guard<std::mutex> lck(m);
-    for (auto& game: map_games) {
-        delete game.second;
-    }
     map_games.clear();
 }
 
